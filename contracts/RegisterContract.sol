f0dbe414026aac314d755e547494a08ae289f9f5
Ervin Dimitri <ervinluis84@gmail.com>
2021-12-09 03:42:50 +0200

    Update RegisterContract.sol

--- contracts/RegisterContract.sol
+++ contracts/RegisterContract.sol
@@ -1,9 +1,12 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.21 <0.7.0;
+// pragma solidity 0.7.2;
+pragma experimental ABIEncoderV2;
 
 contract RegisterContract{
     address public owner;
     mapping (address => Podcast[]) public podsFromArtist;
+
+    mapping (address => uint) public count;
 
     struct Podcast{
         string name;
@@ -17,9 +20,19 @@ contract RegisterContract{
     } 
 
     function newPodcast(string memory _name, string memory _content) public returns( bool isSuccessed){
-        Podcast memory pc = Podcast(_name, now, _content);
+        Podcast memory pc = Podcast(_name, block.timestamp, _content);
         podsFromArtist[msg.sender].push(pc);
-        emit NewPodcast( msg.sender, _name, _content, now);
-        return true;
+        emit NewPodcast( msg.sender, _name, _content, block.timestamp);
+        isSuccessed = true;
+    }
+
+    function allPodcastsFromArtist(address _account)public view returns(string [] memory){
+        Podcast [] memory pc = podsFromArtist[_account];
+        string [] memory data = new string[](2);
+        // data = [pc.name, string(pc.uploadTime),pc.content];
+        data[0] = pc[0].name;
+        // data[1] = ""+pc.uploadTime;
+        data[1] = pc[0].content;
+        return data;
     }
 }

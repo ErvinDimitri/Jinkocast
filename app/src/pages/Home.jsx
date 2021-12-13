9c35f56cc4669e8afe3bb40faa9a31ecc74ef3e8
Ervin Dimitri <ervinluis84@gmail.com>
2021-12-13 23:42:57 +0200

    Update Home.jsx

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -1,17 +1,26 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import { newContextComponents } from "@drizzle/react-components";
 import {AudioCardC} from "../components/AudioCardC";
 import {PlayerControls} from "../components/PlayerControls";
-
+import { useSelector } from "react-redux";
 
 export default ({ drizzle, drizzleState }) => {
   const { AccountData, ContractData, ContractForm } = newContextComponents;
+  const [podcast, setPodcast] = useState(null);
+  const {playing} = useSelector( state => state.musicReducers);
 
+  useEffect(()=>{
+    setPodcast(playing);
+  }, [playing])
+  
   
   return (
     <div className="Home">
       <AudioCardC />
-      <PlayerControls />
+      {
+        podcast&& <PlayerControls podcast={podcast}/>
+      }
+      
     </div>
   );
 };
